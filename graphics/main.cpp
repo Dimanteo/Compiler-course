4aba8c59024cf94f07ba94452c46f700b1475dea
dimanteo <dikolobay@gmail.com>
2022-09-30 21:49:11 +0300

    [graphics] Integer arithmetics
    Changed all floating point operations to integer operations with fixed
    precision

--- graphics/main.cpp
+++ graphics/main.cpp
@@ -1,8 +1,21 @@
 #include <SFML/Graphics.hpp>
 
-#define ITER_MAX 500
+typedef long long int coord_t;
 
-typedef double coord_t;
+const size_t ITER_MAX = 100;
+const coord_t PRECISION = 1000000;
+
+// Make int from floating point number
+coord_t inline getcoord(double x) { return (coord_t)(x * PRECISION); }
+
+// 
+coord_t inline mul(coord_t l, coord_t r) {
+    return l * r / PRECISION;
+}
+
+coord_t inline div(coord_t l, coord_t r) {
+    return l / r * PRECISION;
+}
 
 struct MandelbrotSet {
     coord_t x_max;
@@ -17,16 +30,16 @@ struct MandelbrotSet {
 size_t computePointIter(coord_t re, coord_t im) {
     size_t iter_limit = ITER_MAX;
     coord_t inf_border = 2;
-    // z = z^2 + c, where c = re + i * im
+    // z = z^2 + c, where c = re + j * im
     coord_t z_re = re;
     coord_t z_im = im;
     for (size_t n = 0; n < iter_limit; n++) {
-        coord_t sq_z_re = z_re * z_re;
-        coord_t sq_z_im = z_im * z_im;
-        if (sq_z_re + sq_z_re > inf_border * inf_border) {
+        coord_t sq_z_re = mul(z_re, z_re);
+        coord_t sq_z_im = mul(z_im, z_im) ;
+        if (sq_z_re + sq_z_re > inf_border * inf_border * PRECISION) {
             return n;
         }
-        z_im = 2 * z_im * z_re + im;
+        z_im = 2 * mul(z_im, z_re) + im;
         z_re = sq_z_re - sq_z_im + re;
     }
     return ITER_MAX;
@@ -43,10 +56,10 @@ sf::Color getColorForN(uint32_t n) {
 
 void drawMandelbrotSet(sf::VertexArray &points, size_t width, size_t height, MandelbrotSet *mdb_set) {
     // Note that x_min and y_min are negative
-    coord_t x_min = mdb_set->x_center + mdb_set->x_min * mdb_set->scale;
-    coord_t x_max = mdb_set->x_center + mdb_set->x_max * mdb_set->scale;
-    coord_t y_min = mdb_set->y_center + mdb_set->y_min * mdb_set->scale;
-    coord_t y_max = mdb_set->y_center + mdb_set->y_max * mdb_set->scale;
+    coord_t x_min = mdb_set->x_center + mul(mdb_set->x_min, mdb_set->scale);
+    coord_t x_max = mdb_set->x_center + mul(mdb_set->x_max, mdb_set->scale);
+    coord_t y_min = mdb_set->y_center + mul(mdb_set->y_min, mdb_set->scale);
+    coord_t y_max = mdb_set->y_center + mul(mdb_set->y_max, mdb_set->scale);
 
     coord_t dx = (x_max - x_min) / (width - 1);
     coord_t dy = (y_max - y_min) / (height - 1);
@@ -64,24 +77,24 @@ void handleKey(const sf::Event &event, MandelbrotSet *mdb_set) {
     switch (event.key.code) {
         case sf::Keyboard::A:
             // zoom in
-            mdb_set->scale /= 1.1;
+            mdb_set->scale = mul(mdb_set->scale, getcoord(0.9));
             break;
         case sf::Keyboard::S:
             // zoom out
-            mdb_set->scale *= 1.1;
+            mdb_set->scale = mul(mdb_set->scale, getcoord(1.1));
             break;
         case sf::Keyboard::Up:
             // move center up
-            mdb_set->y_center += 0.1 * mdb_set->scale;
+            mdb_set->y_center += mul(getcoord(0.1), mdb_set->scale);
             break;
         case sf::Keyboard::Down:
-            mdb_set->y_center -= 0.1 * mdb_set->scale;
+            mdb_set->y_center -= mul(getcoord(0.1), mdb_set->scale);
             break;
         case sf::Keyboard::Left:
-            mdb_set->x_center -= 0.1 * mdb_set->scale;
+            mdb_set->x_center -= mul(getcoord(0.1), mdb_set->scale);
             break;
         case sf::Keyboard::Right:
-            mdb_set->x_center += 0.1 * mdb_set->scale;
+            mdb_set->x_center += mul(getcoord(0.1), mdb_set->scale);
             break;
         default:
             break;
@@ -97,11 +110,11 @@ int main() {
 
     sf::VertexArray points;
     MandelbrotSet mdb_set = {
-        .x_max = 1.2,
-        .x_min = -2.2,
-        .y_max = 1.7,
-        .y_min = -1.7,
-        .scale = 1,
+        .x_max = getcoord(1.2),
+        .x_min = getcoord(-2.2),
+        .y_max = getcoord(1.7),
+        .y_min = getcoord(-1.7),
+        .scale = getcoord(1),
         .x_center = 0,
         .y_center = 0
     };
